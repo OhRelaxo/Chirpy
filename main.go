0bdcb9267cb245b870b2fe514c0de54628f562c8
marcel <marcel.ba@proton.me>
2025-09-09 16:00:26 +0200

    added some stuff

--- main.go
+++ main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const port = "8080"
 	const rootPath = "."
 
+	apiCfg := apiConfig{fileserverHits: atomic.Int32{}}
 	mux := http.NewServeMux()
 	mux.HandleFunc("/healthz", handlerReadiness)
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(rootPath))))
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(rootPath)))))
+	mux.Handle("/metrics", apiCfg.middlewareMetricsOut())
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -28,3 +35,21 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	_, err := w.Write([]byte(http.StatusText(http.StatusOK)))
 	log.Printf("log in <handlerReadiness> at w.Write:\n%v", err)
 }
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	cfg.fileserverHits.Add(1)
+	return next
+}
+
+func (cfg *apiConfig) middlewareMetricsOut(next http.Handler) func(http.ResponseWriter, *http.Request) {
+
+}
+
+/*
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		s := strconv.Itoa(int(cfg.fileserverHits.Load()))
+		w.Write([]byte(s))
+	}
+*/
