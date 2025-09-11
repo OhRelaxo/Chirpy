43f1e4d105c7d62d0a9744c76c73dfb12bd78d27
marcel <marcel.ba@proton.me>
2025-09-11 15:59:20 +0200

    rebased

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -72,43 +71,4 @@ func handlerReadiness(w http.ResponseWriter, _ *http.Request) {
 	if err != nil {
 		log.Printf("error in <handlerReadiness> at w.Write: %v", err)
 	}
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		log.Println("adding hits")
-		cfg.fileserverHits.Add(int32(1))
-		next.ServeHTTP(w, r)
-	}
-	return http.HandlerFunc(handler)
-}
-
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, _ *http.Request) {
-	log.Println("Outputting Hits")
-	w.Header().Add("Content-Type", "text/html")
-	w.WriteHeader(http.StatusOK)
-	_, err := w.Write([]byte(fmt.Sprintf("<html>\n  <body>\n    <h1>Welcome, Chirpy Admin</h1>\n    <p>Chirpy has been visited %d times!</p>\n  </body>\n</html>", cfg.fileserverHits.Load())))
-	if err != nil {
-		log.Printf("error in <handlerMetrics at w.Write: %v", err)
-	}
-}
-
-func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	if !cfg.devMode {
-		http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
-		return
-	}
-
-	log.Println("Resetting Hits")
-	cfg.fileserverHits.Store(int32(0))
-
-	if err := cfg.db.ResetUsers(r.Context()); err != nil {
-		log.Printf("error in <handlerReset> at db.ResetUsers: %v", err)
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	_, _ = w.Write([]byte(http.StatusText(http.StatusOK)))
 }
