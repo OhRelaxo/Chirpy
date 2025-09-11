0910ef5f329d227dd4b15030dc49fbcd37c121e9
marcel <marcel.ba@proton.me>
2025-09-11 09:43:37 +0200

    added ValidateChirp endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +10,14 @@ import (
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+}
+
+type jsonError struct {
+	Error string `json:"error"`
+}
+
+type jsonValid struct {
+	Valid bool `json:"valid"`
 }
 
 func main() {
@@ -24,6 +33,7 @@ func main() {
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServer))
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.Handle("POST /api/validate_chirp", apiCfg.middlewareMetricsInc(http.HandlerFunc(handlerValidateChirp)))
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -40,7 +50,7 @@ func handlerReadiness(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte(http.StatusText(http.StatusOK)))
 	if err != nil {
-		log.Printf("log in <handlerReadiness> at w.Write:\n%v", err)
+		log.Printf("error in <handlerReadiness> at w.Write:\n%v", err)
 	}
 }
 
@@ -59,7 +69,7 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte(fmt.Sprintf("<html>\n  <body>\n    <h1>Welcome, Chirpy Admin</h1>\n    <p>Chirpy has been visited %d times!</p>\n  </body>\n</html>", cfg.fileserverHits.Load())))
 	if err != nil {
-		log.Printf("log in <handlerMetrics at w.Write:\n%v", err)
+		log.Printf("error in <handlerMetrics at w.Write:\n%v", err)
 	}
 }
 
@@ -70,6 +80,58 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte(http.StatusText(http.StatusOK)))
 	if err != nil {
-		log.Printf("log in <middlewareMetricsReset> at w.Write:\n%v", err)
+		log.Printf("error in <middlewareMetricsReset> at w.Write:\n%v", err)
 	}
+}
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	params := parameters{}
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("error in <handlerValidateChirp> at decoder.Decode:\n%v", err)
+		w.WriteHeader(500)
+		jError := jsonError{Error: "Something went wrong"}
+		rawJson, err := json.Marshal(&jError)
+		if err != nil {
+			log.Printf("error in <handlerValidateChirp> at json.Marshal:\n%v", err)
+		}
+		w.Write(rawJson)
+		return
+	}
+	if len(params.Body) > 140 {
+		log.Println("log in <handlerValidateChrip> Body too long")
+		w.WriteHeader(400)
+		jError := jsonError{Error: "Chirp is too long"}
+		rawJson, err := json.Marshal(&jError)
+		if err != nil {
+			log.Printf("error in <handlerValidateChirp> at json.Marshal:\n%v", err)
+		}
+		w.Write(rawJson)
+		return
+	}
+	if len(params.Body) == 0 {
+		log.Println("log in <handlerValidateChrip> Body too short")
+		w.WriteHeader(400)
+		jError := jsonError{Error: "Chirp is too short or wrong parameter was used"}
+		rawJson, err := json.Marshal(&jError)
+		if err != nil {
+			log.Printf("error in <handlerValidateChirp> at json.Marshal:\n%v", err)
+		}
+		w.Write(rawJson)
+		return
+	}
+	w.WriteHeader(200)
+	jValid := jsonValid{Valid: true}
+	rawJson, err := json.Marshal(&jValid)
+	if err != nil {
+		log.Printf("error in <handlerValidateChirp> at json.Marshal:\n%v", err)
+	}
+	w.Write(rawJson)
 }
